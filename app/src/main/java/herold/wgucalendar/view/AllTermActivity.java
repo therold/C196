9b040ce70ffa7a9911d07c592d6c8f8628bacb17
Tim Herold <heroldt@gmail.com>
2018-06-28 11:25:32 -0700

    Style cleanup, continued

--- app/src/main/java/herold/wgucalendar/view/AllTermActivity.java
+++ app/src/main/java/herold/wgucalendar/view/AllTermActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,68 +20,64 @@ import herold.wgucalendar.data.TermData;
 import herold.wgucalendar.model.Term;
 
 public class AllTermActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
     private Context context = this;
-    private TermData datasource;
-    private ListView lvTerms;
+    private DrawerLayout drawerLayout;
+    private ImageView imgMenu;
     private List<Term> terms;
+    private ListView lvTerms;
+    private NavigationView navigationView;
     private TermAdapter adapter;
+    private TermData termData;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_term);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.all_term);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-
+        toolbar = findViewById(R.id.toolbar);
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
+        imgMenu = findViewById(R.id.imgMenu);
+        lvTerms = findViewById(R.id.lvTerms);
+
+        ViewHelper.setupToolbar(this, toolbar, R.string.all_term);
+        termData = new TermData(this);
+        termData.open();
+        terms = termData.all();
+        adapter = new TermAdapter(this, terms);
+        lvTerms.setAdapter(adapter);
+        lvTerms.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView lv, View v, int position, long id) {
+                viewTerm((Term) lv.getItemAtPosition(position));
+            }
+        });
         navigationView.setNavigationItemSelectedListener(ViewHelper.getNavigationListener(context, drawerLayout));
-        ImageView imgMenu = findViewById(R.id.imgMenu);
         imgMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { drawerLayout.openDrawer(GravityCompat.START); }
         });
-
-        datasource = new TermData(this);
-        datasource.open();
-        lvTerms = findViewById(R.id.lvTerms);
-        terms = datasource.all();
-        adapter = new TermAdapter(this, terms);
-        lvTerms.setAdapter(adapter);
-        lvTerms.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView lv, View v, int position, long id) {
-               Intent intent = new Intent(context, ViewTermActivity.class);
-               intent.putExtra("Term", (Term) lv.getItemAtPosition(position));
-               startActivityForResult(intent, 0);
-           }
-        });
     }
 
-    @Override
-    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == ViewHelper.DATA_SET_CHANGED) {
-            datasource.open();
-            terms = datasource.all();
-            adapter.clear();
-            adapter.addAll(terms);
-        }
+    private void viewTerm(Term term) {
+        Intent intent = new Intent(context, ViewTermActivity.class);
+        intent.putExtra("Term", term);
+        startActivity(intent);
     }
 
     @Override
     protected void onResume() {
-        datasource.open();
+        termData.open();
+        terms = termData.all();
+        adapter.clear();
+        adapter.addAll(terms);
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        datasource.close();
+        termData.close();
         super.onPause();
     }
 }
