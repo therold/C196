3640ac454491120423e3ccb271df0432ed87d200
Tim Herold <heroldt@gmail.com>
2018-06-28 11:32:35 -0700

    Style cleanup, continued

--- app/src/main/java/herold/wgucalendar/view/MainActivity.java
+++ app/src/main/java/herold/wgucalendar/view/MainActivity.java
@@ -1,51 +1,37 @@
 package herold.wgucalendar.view;
 
-import android.app.ListActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
-import java.util.List;
-
 import herold.wgucalendar.R;
-import herold.wgucalendar.data.TermData;
-import herold.wgucalendar.model.Term;
-import herold.wgucalendar.view.AddTermActivity;
-import herold.wgucalendar.view.TermAdapter;
 
 public class MainActivity extends AppCompatActivity {
-    private DrawerLayout drawerLayout;
     private Context context = this;
+    private DrawerLayout drawerLayout;
+    private ImageView imgMenu;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setTitle(R.string.main_title);
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_home);
-
         drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        imgMenu = findViewById(R.id.imgMenu);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        ViewHelper.setupToolbar(this, toolbar, R.string.main_title);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+
         navigationView.setNavigationItemSelectedListener(ViewHelper.getNavigationListener(context, drawerLayout));
-        ImageView imgMenu = findViewById(R.id.imgMenu);
         imgMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { drawerLayout.openDrawer(GravityCompat.START); }
